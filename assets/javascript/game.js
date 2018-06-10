9e7e0b39335e5f23160500ccf2d94542e9dd6bab
Thorley <thorley.eric@gmail.com>
2018-06-10 17:05:37 -0400

    everything but the win condition

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,67 @@
-//basic parameters
-var wins = 0
-var losses = 0
-var currentScore = 0
 
-//need too assign variable numbers to the appropriate div
-//on.ready so the divs load when the game loads
+$(document).ready(function(){
+    //basic parameters
+    var wins = 0
+    var losses = 0
+    var currentScore = 0
+   
+    //function to set 4 random numbers
+    function crystalValues(){
+        one =(Math.floor(Math.random()*11+1))
+        two = Math.floor(Math.random()*11+1)
+        three = Math.floor(Math.random()*11+1)
+        four = Math.floor(Math.random()*11+1)
+        console.log(one, two, three, four)}
 
-    $(document).ready(function(){
-$("#goal").html(goal)
-$("#wins").html("<p> Wins:" + wins + "<br> Losses:" + losses);  //wins loss div
-$("#points").html("<p>Current Score: " + currentScore);  //score counter div
+    //function to set target points
+    function targetScore(){
+        return [Math.floor(Math.random()* 101+19)]
+        } 
+        var goal = targetScore()
+
+    //Game conditions//
+    reset()
+    $("#goal").html("<p> Target Score is: " + goal + "</p>")
+    $("#wins").html("<p> Wins:" + wins + "<br> Losses:" + losses);  //wins loss div
+    $("#points").html("<p>Current Score: " + currentScore);  //score counter div
+    
+    //function to set points per crystal
+    $("#crystalOne").on("click", function(){             //adds points to current points
+        currentScore = currentScore + one
+        $("#points").html(currentScore);
     })
+    $("#crystalTwo").on("click", function(){
+        currentScore = currentScore + two
+        $("#points").html(currentScore);
+    })
+    $("#crystalThree").on("click", function(){
+        currentScore = currentScore + three
+        $("#points").html(currentScore);
+        })
+    $("#crystalFour").on("click", function(){
+        currentScore = currentScore + four
+        $("#points").html(currentScore);
+        })
+    
+    //win conditions
 
-//function to set points per crystal
-var crystal = crystalPoints 
-function crystalPoints(){
-    return [Math.floor(Math.random()*12)]
-} 
-//function to set target points
-var goal = targetScore 
-function targetScore(){
-   return [Math.floor(Math.random()* 130)]
-} 
+    if (currentScore === goal){
+        wins++
+        reset()
+    }
 
-//need the function to assign points from crystal to score
-//$(document).ready(function(){
-  //  $(".crystal").on("click",function(crystalPoints)
-    //})
+    if (currentScore > goal){
+        losses++
+        reset()
+    }
+    //a reset game function
+    
     
 
+    function reset(){
+        goal = targetScore();
+        crystalValues();
+        currentScore = 0
+    }
+})
 
