68eefeb0cf173d940668223ae6ead5b104c3c0c2
Thorley <thorley.eric@gmail.com>
2018-06-10 13:42:16 -0400

    math works for the score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,32 @@
 //basic parameters
 var wins = 0
 var losses = 0
+var currentScore = 0
+
+//need too assign variable numbers to the appropriate div
+//on.ready so the divs load when the game loads
+
+    $(document).ready(function(){
+$("#goal").html(goal)
+$("#wins").html("<p> Wins:" + wins + "<br> Losses:" + losses);  //wins loss div
+$("#points").html("<p>Current Score: " + currentScore);  //score counter div
+    })
+
 //function to set points per crystal
 var crystal = crystalPoints 
-function(crystalPoints){
+function crystalPoints(){
     return [Math.floor(Math.random()*12)]
 } 
 //function to set target points
 var goal = targetScore 
-function(targetScore){
+function targetScore(){
    return [Math.floor(Math.random()* 130)]
 } 
 
 //need the function to assign points from crystal to score
-$(document).ready(function){
+//$(document).ready(function(){
+  //  $(".crystal").on("click",function(crystalPoints)
+    //})
+    
 
-    function(crystalPoints){
-    $(".crystal").on("click",function(){
-      
-    })
-    }
 
-}
-//need too assign variable numbers to the appropriate div
-$("#wins").html("<p> wins:" + wins + "<br> Losses:" + losses);
