66a23d25c0c26ab202eeb74396bd85b3679d204c
Thorley <thorley.eric@gmail.com>
2018-06-10 17:57:03 -0400

    working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     //basic parameters
     var wins = 0
     var losses = 0
-    var currentScore = 0
    
     //function to set 4 random numbers
     function crystalValues(){
@@ -22,51 +21,47 @@ $(document).ready(function(){
     //Game conditions//
     newGame()
 
-    //function to set points per crystal
-    $("#crystalOne").on("click", function(){             //adds points to current points
+    //function to add points when a certain crystal is clicked
+    $("#crystalOne").on("click", function(){             
         currentScore = currentScore + one
-        scoreUpdate()
-    })
+        })
     $("#crystalTwo").on("click", function(){
         currentScore = currentScore + two
-        scoreUpdate()
-    })
+        })
     $("#crystalThree").on("click", function(){
         currentScore = currentScore + three
-       scoreUpdate()
         })
     $("#crystalFour").on("click", function(){
         currentScore = currentScore + four
-        scoreUpdate()
         })
-    
+// function to run conditions when any crystal is clicked
+    $(".crystal").on("click", function(){
+        scoreUpdate()
+    })
+
     //win conditions
+        function scoreUpdate(){
+            if (currentScore == goal){
+                wins++
+                newGame()
+            }
 
+            if (currentScore > goal){
+                losses++
+                newGame()
+            }   
 
-    //a reset game function
-    
-    function scoreUpdate(){
-        if (currentScore == goal){
-            wins++
-            newGame()
+        else {$("#points").html("<p>Current Score: " + currentScore);}          
         }
-        
-       else if (currentScore > goal){
-            losses++
-            newGame()
-        }   
-
-        else {$("#points").html(currentScore);}          
-    }
 
     //Here are the game conditions
-    function newGame(){
-        goal = targetScore();
-        crystalValues();
-        $("#goal").html("<p> Target Score is: " + goal + "</p>")
-        $("#wins").html("<p> Wins:" + wins + "<br> Losses:" + losses);  //wins loss div
-        currentScore = 0
-        $("#points").html("<p>Current Score: " + currentScore);  //score counter div
-    }
+        function newGame(){
+            goal = targetScore();
+            crystalValues();
+            $("#goal").html("<p> Target Score is: " + goal + "</p>")
+            $("#wins").html("<p> Wins: " + wins + "<br> Losses: " + losses);  //wins loss div
+            currentScore = 0
+            $("#points").html("<p>Current Score: " + currentScore);  //score counter div
+        }
 })
 
